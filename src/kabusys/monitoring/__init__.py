f2ecdeca08ef9ad872e55f6a9d359051dc957c9d
ttj-ishida <tetsuziishida@gmail.com>
2026-03-31 16:02:23 +0900

    feat: export SystemMonitor and SystemCheckResult from monitoring package (Issue #37)

--- src/kabusys/monitoring/__init__.py
+++ src/kabusys/monitoring/__init__.py
@@ -1,3 +1,9 @@
 from kabusys.monitoring.monitoring_db import MonitoringDB, init_monitoring_db
+from kabusys.monitoring.system_monitor import SystemCheckResult, SystemMonitor
 
-__all__ = ["MonitoringDB", "init_monitoring_db"]
+__all__ = [
+    "MonitoringDB",
+    "init_monitoring_db",
+    "SystemCheckResult",
+    "SystemMonitor",
+]
