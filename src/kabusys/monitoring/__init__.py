d20ce41d137975eab799216c240a8390f400cc6c
ttj-ishida <tetsuziishida@gmail.com>
2026-04-03 01:13:51 +0900

    feat: export KillSwitch and AlertManager from monitoring package

--- src/kabusys/monitoring/__init__.py
+++ src/kabusys/monitoring/__init__.py
@@ -1,3 +1,5 @@
+from kabusys.monitoring.alert_manager import AlertManager
+from kabusys.monitoring.kill_switch import KillSwitch
 from kabusys.monitoring.monitoring_db import MonitoringDB, init_monitoring_db
 from kabusys.monitoring.monitoring_engine import MonitoringEngine
 from kabusys.monitoring.risk_monitor import RiskCheckResult, RiskMonitor
@@ -5,6 +7,8 @@ from kabusys.monitoring.system_monitor import SystemCheckResult, SystemMonitor
 from kabusys.monitoring.trade_monitor import TradeCheckResult, TradeMonitor
 
 __all__ = [
+    "AlertManager",
+    "KillSwitch",
     "MonitoringDB",
     "init_monitoring_db",
     "SystemMonitor",
